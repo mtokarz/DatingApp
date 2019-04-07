7b1d754486c182e731d1c65b27a5a4c0f220bc49
mtokarz <34172570+mtokarz@users.noreply.github.com>
2019-04-07 16:30:54 +0200

    Implement logging in in the controller

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.DTOs;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,9 +17,13 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+
+        private IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -28,6 +38,37 @@ namespace DatingApp.API.Controllers
             
             await _repo.Register(newUser, user.Password);
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO userData)
+        {
+            User user = await _repo.Login(userData.Username.ToLower(), userData.Password);
+            if(user != null)
+            {
+                return Unauthorized();
+            }
+            var claims = new []
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new 
+            {
+                token = tokenHandler.WriteToken(token)
+            });
+
+            
         }
     }
 }
